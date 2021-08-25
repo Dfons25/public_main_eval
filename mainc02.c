9fc1e4f1a16fc66457f36c4b3b831903823ca9df
David Afonso <davidmgafonso@gmail.com>
2021-08-25 20:49:48 +0100

    Update mainc02.c

--- mainc02.c
+++ mainc02.c
@@ -2,6 +2,7 @@
 #include <limits.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 #include "ex00/ft_strcpy.c"
 #include "ex01/ft_strncpy.c"
@@ -15,7 +16,7 @@
 #include "ex09/ft_strcapitalize.c"
 #include "ex10/ft_strlcpy.c"
 #include "ex11/ft_putstr_non_printable.c"
-#include "ex12/ft_print_memory.c"
+// #include "ex12/ft_print_memory.c"
 
 int main()
 {
@@ -50,7 +51,7 @@ int main()
 		char dest2[100];
 		
 		printf("original\n");
-		memset(dest2, '\0', sizeof(dest));
+		memset(dest2, '\0', sizeof(dest2));
 		strcpy(src2, "This is tutorialspoint.com");
 		strcpy(dest2, src2);
 
@@ -97,11 +98,7 @@ int main()
 	case 2:
 	{
 		printf("****************************Ex02 ft_str_is_alpha ***********************************\n ");
-		printf("Escreva uma função que retorne 1 se a string passada como parâmetro só contiver caracteres\n");
-		printf("alfabéticos e retorne 0 se a função contiver outros tipos de caracteres.\n\n");
-		printf("Ela deverá retornar 1 se str for uma string vazia.\n\n");
-		printf("Sintaxe: int ft_str_is_alpha (char *string); \n \n");
-
+		
 		char *alfabetica_2 = "Olamundo";
 		char *nao_alfabetica_2 = "Ola mundo ";
 		char *vazia_2 = "";
@@ -116,11 +113,6 @@ int main()
 	}
 	case 3:
 	{
-		printf("****************************Ex03 ft_str_is_numeric ***********************************\n\n ");
-		printf("Escreva uma função que retorne 1 se a string passada como parâmetro só contiver números \n");
-		printf("e retorne 0 se a função contiver outros tipos de caracteres.\n\n");
-		printf("Ela deverá retornar 1 se str for uma string vazia.\n\n");
-		printf("Sintaxe:  int ft_str_is_numeric(char *str); \n \n");
 		char *vazia_3 = "";
 		char *alfanumerico_3 = "1234567890";
 		char *nao_alfanumerico_3 = "123456789o";
@@ -136,12 +128,6 @@ int main()
 	}
 	case 4:
 	{
-		printf("****************************Ex04 ft_str_is_lowercase ***********************************\n\n ");
-		printf("Escreva uma função que retorne 1 se a string passada como parâmetro só contiver caracteres alfabéticos minúsculos \n");
-		printf("e retorne 0 se a função contiver outros tipos de caracteres.\n\n");
-		printf("Ela deverá retornar 1 se str for uma string vazia.\n\n");
-		printf("Sintaxe:  int ft_str_is_lowercase(char *str); \n \n");
-
 		char *vazia_4 = "";
 		char *minusculas_4 = "olamundo";
 		char *nao_minusculas_4 = "ola mundo";
@@ -159,12 +145,6 @@ int main()
 
 	case 5:
 	{
-		printf("****************************Ex05 ft_str_is_uppercase ***********************************\n\n ");
-		printf("Escreva uma função que retorne 1 se a string passada como parâmetro só contiver caracteres alfabéticos maiúsculos \n");
-		printf("e retorne 0 se a função contiver outros tipos de caracteres.\n\n");
-		printf("Ela deverá retornar 1 se str for uma string vazia.\n\n");
-		printf("Sintaxe:  int ft_str_is_uppercase(char *str); \n \n");
-
 		char *vazia_5 = "";
 		char *maiusculas_5 = "OLAMUNDO";
 		char *nao_maiusculas_5 = "OLA MUNDO";
@@ -182,12 +162,6 @@ int main()
 
 	case 6:
 	{
-		printf("**************************** Ex06 ft_str_is_printable ***********************************\n\n ");
-		printf("Escreva uma função que retorne 1 se a string passada como parâmetro só contiver caracteres imprimíveis \n");
-		printf("e retorne 0 se a função contiver outros tipos de caracteres.\n\n");
-		printf("Ela deverá retornar 1 se str for uma string vazia.\n\n");
-		printf("Sintaxe:  int ft_str_is_printable(char *str); \n \n");
-
 		char *vazia_6 = "";
 		char *nao_imprimivel_6 = "OLAMU\x19NDO";
 		char *imprimivel_6 = "OLA MUNDO";
@@ -203,8 +177,6 @@ int main()
 	case 7:
 	{
 		printf("**************************** Ex07 ft_strupcase ***********************************\n\n ");
-		printf("Escreva uma função que deixe todas as letras em maiúsculo. \n");
-		printf("Sintaxe:  int ft_strupcase(char *str); \n \n");
 
 		char minusculas_7[50] = "Fez o exercicio Certo!!!";
 
@@ -217,8 +189,6 @@ int main()
 	case 8:
 	{
 		printf("**************************** Ex08 ft_strlowcase ***********************************\n\n ");
-		printf("Escreva uma função que deixe todas as letras em minusculas. \n");
-		printf("Sintaxe:  int ft_strlowcase(char *str); \n \n");
 
 		char maiusculas_8[50] = "FEZ O EXERCICIO CERTO!!!";
 
@@ -230,14 +200,14 @@ int main()
 	}
 	case 9:
 	{
-		printf("**************************** Ex09 ft_strcapitalize ***********************************\n\n ");
-		printf("Escreva uma função que deixe a primeira letra de cada palavra em maiúsculo e o resto da palavra em minúsculo.\n");
-		printf("* Uma palavra é uma sequência de caracteres alfanuméricos. \n");
-		printf("* Ela deverá retornar str. \n");
-		printf("Sintaxe:  char *ft_strcapitalize(char *str); \n \n");
-
 		char frase_9[100] = "o exerCicio esta-certo!!! 42escola ";
 		printf("\n %s \n", ft_strcapitalize(frase_9));
+
+		char frase_92[100] = "oi, tudo bem? 42palavras quarenta-e-duas; cinquenta+e+um";
+		printf("\n %s \n", ft_strcapitalize(frase_92));
+
+		char frase_93[100] = "e";
+		printf("\n %s \n", ft_strcapitalize(frase_93));
 
 		printf("\n\n*******************************************************************\n\n\n");
 
@@ -266,13 +236,8 @@ int main()
 	case 11:
 	{
 		printf("**************************** Ex11 ft_putstr_non_printable ***********************************\n\n ");
-		printf("Escreva uma função que mostre uma string de caracteres na tela. Se essa string contiver caracteres não imprimíveis, eles devem ser mostrados na forma hexadecimal (em minúsculo) precedidos por um \"backslash\".\n");
-		printf(" \n");
-		printf(" \n");
-		printf("Sintaxe:   void ft_putstr_non_printable(char *str); \n\n");
-
-		char a[] = {1, 26, 27};
-		printf("Solucao: \n\\01\\1a\\1b\n");
+		char a[] = "Oi\nvoce esta bem?";
+		printf("Solucao: Oi\\0avoce esta bem?");
 		printf("Resultado:\n");
 		ft_putstr_non_printable(a);
 
@@ -280,20 +245,20 @@ int main()
 
 		break;
 	}
-	case 12:
-	{
-		printf("**************************** Ex11 ft_putstr_non_printable ***********************************\n\n ");
-		printf("Escreva uma função que mostre uma string de caracteres na tela. Se essa string contiver caracteres não imprimíveis, eles devem ser mostrados na forma hexadecimal (em minúsculo) precedidos por um \"backslash\".\n");
-		printf(" \n");
-		printf(" \n");
-		printf("Sintaxe:   void ft_putstr_non_printable(char *str); \n\n");
+	// case 12:
+	// {
+	// 	printf("**************************** Ex11 ft_putstr_non_printable ***********************************\n\n ");
+	// 	printf("Escreva uma função que mostre uma string de caracteres na tela. Se essa string contiver caracteres não imprimíveis, eles devem ser mostrados na forma hexadecimal (em minúsculo) precedidos por um \"backslash\".\n");
+	// 	printf(" \n");
+	// 	printf(" \n");
+	// 	printf("Sintaxe:   void ft_putstr_non_printable(char *str); \n\n");
 
-		int n12 = 234;
-		int *pointer12 = &n12;
-		ft_print_memory(pointer12, 8);
-		printf("\n\n*******************************************************************\n\n\n");
+	// 	int n12 = 234;
+	// 	int *pointer12 = &n12;
+	// 	ft_print_memory(pointer12, 8);
+	// 	printf("\n\n*******************************************************************\n\n\n");
 
-		break;
-	}
+	// 	break;
+	// }
 	}
 }
